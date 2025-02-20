c022485ca6fa1e418bb97277f6c7d977e1a76bbe
rao-shreya <shreyarao2026@gmail.com>
2025-02-20 14:52:16 +0530

    promises

--- Day1/p6.js
+++ Day1/p6.js
@@ -1,15 +1,14 @@
 const fs = require('fs');
 
-const write=()=>{
-    const data="I am new file"
-    fs.writeFile("./data.txt",data,(err)=>{
+const write=(data)=>{
+    fs.writeFile("./mydir/data.txt",data,(err)=>{
         if(err)
             console.log("Error Writing File",err);
         else
         console.log("File Written Successfully");
     });
 }
-write("I am new data");
+write("Shreya");
 
 
 
